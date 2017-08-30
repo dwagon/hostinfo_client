75d1cc50e112ea58acc1b21bb536eb3e5cff505d
Dougal Scott <dougal.scott@gmail.com>
2017-08-30 17:43:45 +1000

    Handle data and params

--- hostinfo_client/__init__.py
+++ hostinfo_client/__init__.py
@@ -1,3 +1,5 @@
+import os
+import json
 import sys
 import requests
 
@@ -6,9 +8,21 @@ hostinfourl = 'http://localhost:8000'
 
 
 ##############################################################################
-def hostinfo_cmd(cmd, verb, url=None):
+def get_origin(origin=None):
+    if origin is not None:
+        return origin
+    try:
+        user = os.environ['USER']
+    except:
+        user = 'unknown'
+    origin = "{} by {}".format(sys.argv[0], user)
+    return origin
+
+
+##############################################################################
+def hostinfo_cmd(cmd, verb, url, data={}, params={}):
     hi_url = '%s/api/%s' % (hostinfourl, url)
-    r = cmd(hi_url)
+    r = cmd(hi_url, data=json.dumps(data), params=params)
     if r.status_code != 200:
         sys.stderr.write("Failed to {} {} ({})\n".format(verb, url, r.status_code))
         return None
@@ -16,17 +30,17 @@ def hostinfo_cmd(cmd, verb, url=None):
 
 
 ##############################################################################
-def hostinfo_post(url=None):
-    return hostinfo_cmd(requests.post, 'post', url)
+def hostinfo_post(url=None, data={}, params={}):
+    return hostinfo_cmd(requests.post, 'post', url, data=data, params=params)
 
 
 ##############################################################################
-def hostinfo_get(url=None, payload={}):
-    return hostinfo_cmd(requests.get, 'get', url)
+def hostinfo_get(url=None, data={}, params={}):
+    return hostinfo_cmd(requests.get, 'get', url, data=data, params=params)
 
 
 ##############################################################################
-def hostinfo_delete(url=None):
-    return hostinfo_cmd(requests.delete, 'delete', url)
+def hostinfo_delete(url=None, data={}, params={}):
+    return hostinfo_cmd(requests.delete, 'delete', url, data=data, params=params)
 
 # EOF

6a128ea897a553c59c2fcf613b3845b88b5c6a14
Dougal Scott <dougal.scott@gmail.com>
2017-10-05 17:44:22 +0800

    Much testing

--- tests/test_hostinfo.py
+++ tests/test_hostinfo.py
@@ -55,6 +55,72 @@ class TestHostinfo(unittest.TestCase):
 
     ##########################################################################
     @responses.activate
+    def test_aliases(self):
+        """ Test calling hostinfo with alaises """
+        responses.add(
+            responses.GET, "{}/api/query/deadbeef".format(hostinfourl),
+            json={'status': '200', 'result': 'ok', 'hosts': [{'hostname': 'deadbeef'}]},
+            status=200
+            )
+        responses.add(
+            responses.GET, "{}/api/host/deadbeef/alias".format(hostinfourl),
+            json={"aliases": [{"alias": "livecow", "host": "deadbeef"}], "result": "ok"},
+            status=200
+            )
+        rc = hostinfo.main(['--aliases', 'deadbeef'])
+        self.assertEqual(rc, 0)
+        self.assertEqual(len(responses.calls), 2)
+        output = sys.stdout.getvalue()
+        self.assertIn("deadbeef", output)
+        self.assertIn("Aliases: livecow", output)
+
+    ##########################################################################
+    @responses.activate
+    def test_origin_date(self):
+        """ Test calling hostinfo with origin and dates """
+        responses.add(
+            responses.GET, "{}/api/query/deadbeef".format(hostinfourl),
+            json={'status': '200', 'result': 'ok', 'hosts': [{'hostname': 'deadbeef'}]},
+            status=200
+            )
+        responses.add(
+            responses.GET, "{}/api/host/deadbeef/".format(hostinfourl),
+            json={
+                "host": {
+                    "hostname": "deadbeef",
+                    "origin": "source",
+                    "createdate": "1999-12-31",
+                    "modifieddate": "2000-01-01",
+                    "keyvalues": {
+                        "king": [
+                            {
+                                "value": "william",
+                                "origin": "france",
+                                "createdate": "1066-12-25",
+                                "modifieddate": "1087-09-09"
+                                }
+                            ]
+                        }
+                    },
+                "result": "ok"
+                },
+            status=200
+            )
+        rc = hostinfo.main(['--origin', '--date', '-p', 'king', 'deadbeef'])
+        self.assertEqual(rc, 0)
+        self.assertEqual(len(responses.calls), 2)
+        output = sys.stdout.getvalue()
+        self.assertIn("deadbeef", output)
+        self.assertIn("[Origin: source]", output)
+        self.assertIn("Created: 1999-12-31", output)
+        self.assertIn("Modified: 2000-01-01", output)
+        self.assertIn("king=william", output)
+        self.assertIn("Created: 1066-12-25", output)
+        self.assertIn("Modified: 1087-09-09", output)
+        self.assertIn("[Origin: france]", output)
+
+    ##########################################################################
+    @responses.activate
     def test_print_val(self):
         """ Test calling hostinfo with printing a value """
         responses.add(
@@ -67,8 +133,10 @@ class TestHostinfo(unittest.TestCase):
             json={
                 'status': '200', 'result': 'ok',
                 'host': {
-                    'keyvalues':
-                        {'keyname': [{'key': 'keyname', 'value': 'bar'}]}
+                    'keyvalues': {
+                        'keyname': [{'key': 'keyname', 'value': 'bar'}],
+                        'nother': [{'key': 'nother', 'value': 'baz'}]
+                        }
                     }
                 },
             status=200
@@ -79,6 +147,40 @@ class TestHostinfo(unittest.TestCase):
         output = sys.stdout.getvalue()
         self.assertIn("deadbeef", output)
         self.assertIn("keyname=bar", output)
+        self.assertNotIn("baz", output)
+
+    ##########################################################################
+    @responses.activate
+    def test_showall(self):
+        """ Test calling hostinfo with showall """
+        responses.add(
+            responses.GET, "{}/api/query/deadbeef".format(hostinfourl),
+            json={'status': '200', 'result': 'ok', 'hosts': [{'hostname': 'deadbeef'}]},
+            status=200
+            )
+        responses.add(
+            responses.GET, "{}/api/host/deadbeef/".format(hostinfourl),
+            json={
+                'status': '200', 'result': 'ok',
+                'host': {
+                    'keyvalues': {
+                        'keyname': [
+                            {'key': 'keyname', 'value': 'foo'},
+                            {'key': 'keyname', 'value': 'bar'}
+                            ],
+                        'nother': [{'key': 'nother', 'value': 'baz'}]
+                        }
+                    }
+                },
+            status=200
+            )
+        rc = hostinfo.main(['--showall', 'deadbeef'])
+        self.assertEqual(rc, 0)
+        self.assertEqual(len(responses.calls), 2)
+        output = sys.stdout.getvalue()
+        self.assertIn("deadbeef", output)
+        self.assertIn("keyname: bar,foo", output)
+        self.assertIn("nother: baz", output)
 
 ##############################################################################
 if __name__ == "__main__":
